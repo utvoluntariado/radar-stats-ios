a571e2954d6b39a88ff0219dd13205d9cb7533dc
Jorge J. Ramos <jorge.ramos@me.com>
2020-09-23 16:19:34 +0200

    Add Swinject injector class and prefix operator

---
+++ RadarCOVIDStats/Infrastructure/Injection/Injector.swift
@@ -0,0 +1,29 @@
+//
+//  Injector.swift
+//  RadarCOVIDStats
+//
+//  Original idea by Pedro José Pereira Vieito
+//  Created by Jorge J. Ramos on 23/09/2020.
+//
+
+import Foundation
+import Swinject
+
+final class Injector {
+    static let shared = Injector()
+    internal var currentContainer = Container()
+
+    init() {
+
+    }
+}
+
+prefix operator <~
+prefix func <~<Injection>(right: Injection.Type) -> Injection {
+    if let registeredInjection = Injector.shared.currentContainer.resolve(Injection.self) {
+        return registeredInjection
+    } else {
+        fatalError("Cannot resolve \(Injection.self)")
+    }
+}
+
