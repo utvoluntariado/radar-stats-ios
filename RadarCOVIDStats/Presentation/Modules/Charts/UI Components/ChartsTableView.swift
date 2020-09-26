04bb652221ba1cc6fe9c08b5f3f3f76ae2abfd12
Jorge J. Ramos <jorge.ramos@me.com>
2020-09-26 09:00:39 +0200

    Prepare basic chart table cells to receive stats

--- RadarCOVIDStats/Presentation/Modules/Charts/UI Components/ChartsTableView.swift
+++ RadarCOVIDStats/Presentation/Modules/Charts/UI Components/ChartsTableView.swift
@@ -11,7 +11,27 @@ import UIKit
 final class ChartsTableView: UITableView {
     private var modelset: Stats!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        delegate = self
+        dataSource = self
+    }
+
     func update(modelset: Stats) {
         self.modelset = modelset
+        reloadData()
+    }
+}
+
+
+extension ChartsTableView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ChartsTableViewCell.reuseIndentifier, for: indexPath) as! ChartsTableViewCell
+        cell.bind(modelset: modelset, for: ChartType(rawValue: indexPath.row) ?? .unknown)
+        return cell
     }
 }
--- RadarCOVIDStats/Presentation/Modules/Charts/UI Components/ChartsTableViewCell.swift
+++ RadarCOVIDStats/Presentation/Modules/Charts/UI Components/ChartsTableViewCell.swift
@@ -9,6 +9,19 @@
 import UIKit
 import Charts
 
+enum ChartType: Int {
+    case today
+    case lastWeek
+    case daily
+    case unknown
+}
+
 final class ChartsTableViewCell: UITableViewCell {
+    static let reuseIndentifier = "ChartCell"
+
     @IBOutlet weak var chartView: PieChartView!
+
+    func bind(modelset: Stats, for chartType: ChartType) {
+        
+    }
 }
