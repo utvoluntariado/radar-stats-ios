ee1ee0602be36d63395a48e0dd7292cdaf0998c4
Jorge J. Ramos <jorge.ramos@me.com>
2020-09-29 16:57:35 +0200

    Update some file comment headers

--- RadarCOVIDStats/Network/HTTPClient/HTTPClient.swift
+++ RadarCOVIDStats/Network/HTTPClient/HTTPClient.swift
@@ -3,7 +3,7 @@
 //  RadarCOVIDStats
 //
 //  Original idea by Pedro José Pereira Vieito
-//  Created by Jorge Juan Ramos Garnero on24/09/2020.
+//  Created by Jorge Juan Ramos Garnero on 24/09/2020.
 //
 
 import Foundation
