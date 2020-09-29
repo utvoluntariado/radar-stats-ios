67771aae4fc6350c72a7a8284e16c7a2f851a14b
Jorge J. Ramos <jorge.ramos@me.com>
2020-09-29 10:25:12 +0200

    Add actions to visit github repositories

--- RadarCOVIDStats/Presentation/Modules/About/UI Components/AboutDataTableViewCell.swift
+++ RadarCOVIDStats/Presentation/Modules/About/UI Components/AboutDataTableViewCell.swift
@@ -10,5 +10,14 @@ import UIKit
 
 final class AboutDataTableViewCell: UITableViewCell {
     static let reuseIndentifier = "AboutDataCell"
-}
 
+    @IBAction func didTapRadarCovidStatsDataButton(_ sender: UIButton) {
+        guard let radarCovidStatsURL = URL(string: "https://github.com/pvieito/RadarCOVID-STATS") else { return }
+        UIApplication.shared.open(radarCovidStatsURL)
+    }
+
+    @IBAction func didTapRadarCovidStatsiOSButton(_ sender: UIButton) {
+        guard let radarCovidStatsiOSURL = URL(string: "https://github.com/jorgej-ramos/ios-radarcovid-stats") else { return }
+        UIApplication.shared.open(radarCovidStatsiOSURL)
+    }
+}
